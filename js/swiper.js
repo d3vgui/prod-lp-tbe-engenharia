b993e88791302ed051191c0fdb1f209611f254c3
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-08-05 22:14:14 -0300

    feature: adiciona top-header, carrosseis e ajustes no footer

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,120 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 20,
-  // autoplay: {
-  //   delay: 4000,
-  //   disableOnInteraction: false,
-  // },
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   speed: 800,
   pagination: {
     el: ".swiper-pagination",
     type: "progressbar",
   },
+  breakpoints: {
+    // telas até 480px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // telas a partir de 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // telas a partir de 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper4", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  speed: 800,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  breakpoints: {
+    // telas até 480px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // telas a partir de 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // telas a partir de 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper5", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  speed: 800,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  breakpoints: {
+    // telas até 480px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // telas a partir de 768px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // telas a partir de 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper6", {
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  speed: 800,
 });
 
 var swiper = new Swiper(".mySwiper2", {
